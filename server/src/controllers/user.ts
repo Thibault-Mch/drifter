ea5dfebd67f2a6c72fc6d26daa35d0c94579a489
Thibault <thibault.manchon@hotmail.fr>
2022-06-23 01:05:34 +0200

    ts config

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,8 @@
+
+export default async (name: string) => {
+  if (name === "Heisenberg") {
+    console.log("You're right 👍");
+  } else {
+    console.log("You're wrong 👎");
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 
 const express = require("express");
-
+import user from './controllers/user'
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+user('henri')
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
