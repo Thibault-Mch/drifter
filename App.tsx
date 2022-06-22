9713b962ce2b28cec9cd0ae7750e5499b402626b
Thibault <thibault.manchon@hotmail.fr>
2022-06-22 23:40:36 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect } from 'react';
 export default function App() {
+  const fetchApi = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api');
+      const json = await response.json();
+      console.log(json)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log("hello")
+    }
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Etienne</Text>
       <StatusBar style="auto" />
     </View>
   );
