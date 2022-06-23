ead6c26bc0f80a69ada399c7990497a95ac47c92
Thibault <thibault.manchon@hotmail.fr>
2022-06-23 08:35:02 +0200

    moved evth to config file and nodemon

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,11 +1,11 @@
 
 const express = require("express");
-import user from './controllers/user'
+import user from '../controllers/user'
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-user('henri')
+user('Heisenberg')
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
