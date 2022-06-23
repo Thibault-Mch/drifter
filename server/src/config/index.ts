44b479a56702df0f7fe54dcba563d3a0d236ab2e
Thibault <thibault.manchon@hotmail.fr>
2022-06-23 10:18:48 +0200

    connecting to mongo db

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,11 +1,32 @@
 
 const express = require("express");
-import user from '../controllers/user'
+const mongoose = require("mongoose");
+
+import user from '../controllers/user.controllers'
 const PORT = process.env.PORT || 3001;
 
+const dotenv = require("dotenv")
+dotenv.config()
 const app = express();
 
 user('Heisenberg')
+
+
+console.log(process.env.MONGO_URL)
+
+mongoose.connect(process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
