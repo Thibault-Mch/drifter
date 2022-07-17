cd3f7c81f354bf2f7604dbe69948e04aee8d537d
Thibault <thibault.manchon@hotmail.fr>
2022-07-17 22:35:10 +0200

    fixing ts

--- client/src/components/atoms/InputLine.tsx
+++ client/src/components/atoms/InputLine.tsx
@@ -1,15 +1,25 @@
-import React, { useState, FC } from "react";
+import React, { useState, useEffect, FC } from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 const UselessTextInput: FC = () => {
-  const [text, onChangeText] = useState("Useless Text");
-
+  const [text, onChangeText] = useState<string>("Useless Text");
+  const [number, onChangeNumber] = useState<string>("");
+  useEffect(() => {
+    onChangeText('hello')
+  }, []);
   return (
     <SafeAreaView>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
       />
     </SafeAreaView>
   );
