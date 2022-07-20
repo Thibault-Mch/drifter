11f4f8316eaba887ccf3c37d694008c6821eaf71
Thibault <thibault.manchon@hotmail.fr>
2022-07-20 10:15:59 +0200

    starting react hook forms

--- client/src/views/Registration.tsx
+++ client/src/views/Registration.tsx
@@ -1,30 +1,32 @@
-import { Text, View, Button } from 'react-native'
 import React, { useState } from 'react'
-import api from '../api/index'
-
+import { Text, View, Button } from 'react-native'
+// import api from '../api/index'
+import { IUser } from '../../../server/src/interfaces/user.interface'
+import { useForm } from 'react-hook-form'
 import InputLine from '../components/atoms/InputLine'
 
 const Registration = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+  const { control, handleSubmit } = useForm<IUser>()
+
   const [isSignUp, setIsSignUp] = useState(true)
 
-  const sendRegistration = async () => {
-    if (isSignUp) {
-      await api.signup({ username, password, email })
-    } else {
-      await api.login({ email, password })
-    }
+  const sendRegistration = async (data: IUser) => {
+    console.log(data)
+    // if (isSignUp) {
+    //   await api.signup({ username, password, email })
+    // } else {
+    //   await api.login({ email, password })
+    // }
   }
+
   return (
     <View>
       <Text>Registration</Text>
-      <InputLine label='Your email' placeholder='Email' onChangeInput={setEmail} />
-      {isSignUp && <InputLine label='Your username' placeholder='Username' onChangeInput={setUsername} />}
-      <InputLine label='Your password' placeholder='Password' onChangeInput={setPassword} secureTextEntry />
+      <InputLine label='Your email' placeholder='Email' control={control} />
+      {isSignUp && <InputLine label='Your username' placeholder='Username' control={control} />}
+      <InputLine label='Your password' placeholder='Password' control={control} secureTextEntry />
       {/* onPress takes a function bc the prop type passed to InputLine is void */}
-      <Button title="Login" onPress={async () => await sendRegistration()} />
+      <Button title="Login" onPress={handleSubmit(sendRegistration)} />
       <Button title={isSignUp ? 'Go to login' : 'Go to signup'} onPress={() => setIsSignUp(!isSignUp)} />
     </View>
   )
