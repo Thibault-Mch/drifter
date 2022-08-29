b6b4e515a334d680ba172f0a38e5e70a629b3209
thibault manchon <thibault.manchon@hotmail.fr>
2022-08-29 08:39:55 +0200

    added font, base styling input

--- client/src/styles/colors.ts
+++ client/src/styles/colors.ts
@@ -1,6 +1,7 @@
 export default {
-  baseBlack: "131313",
-  lighterBlack: "202020",
-  tifBlue: "01C5BB",
-  platWhite: "C2B2B4"
+  baseBlack: "#131313",
+  lighterBlack: "#202020",
+  tifBlue: "#01C5BB",
+  platWhite: "#C2B2B4",
+  washedWhite: "rgba(255, 255, 255, 0.5)"
 };
--- client/src/styles/globalStyles.tsx
+++ client/src/styles/globalStyles.tsx
@@ -1,8 +1,13 @@
 import { StyleSheet } from 'react-native';
-
+import colors from './colors';
 export default StyleSheet.create({
   container: {
     padding: 20,
   },
 
+  baseFont: {
+    fontFamily: 'Lato_400Regular',
+    fontSize: 20,
+    color: colors.washedWhite,
+  }
 });
